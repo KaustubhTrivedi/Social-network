846bf0d813ad86953b95b5868792a34064939c4d
Kaustubh Trivedi <kaus12tri@gmail.com>
2022-03-14 14:57:38 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 import firebase from "firebase"
 
 
-const firebaseConfig = firebase.initializeApp( {
-    apiKey: "AIzaSyDqXDksy9QGY7dv52Pf_957AiveiEpff-s",
-    authDomain: "instagram-clone-761ec.firebaseapp.com",
-    projectId: "instagram-clone-761ec",
-    storageBucket: "instagram-clone-761ec.appspot.com",
-    messagingSenderId: "136706128381",
-    appId: "1:136706128381:web:5d44995a52c9708d48c194"
-  });
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyAP_K6-MXoOqulkKemKzxw03SdE20pZD-Y",
+  authDomain: "social-59f2f.firebaseapp.com",
+  projectId: "social-59f2f",
+  // gs://social-59f2f.appspot.com
+  storageBucket: "social-59f2f.appspot.com",
+  messagingSenderId: "244973617294",
+  appId: "1:244973617294:web:9f94aedf45a37c0a0c49df"
+});
 
 
-  
-  const db = firebase.firestore();
-  const auth = firebase.auth();
-  const storage = firebase.storage();
-  export {db,auth,storage};
-  export default firebaseConfig;
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+export { db, auth, storage };
+export default firebaseConfig;
